321ca16ad0d7f963d42476bcab9ca8c746a45b0e
David Swiatkiewiez <davidswiatkiewiez@MacBook-Pro-de-David-Swiatkiewiez.local>
2020-03-05 13:09:48 +0100

    add env databaseurl

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var indexRoutes      = require("./routes/index.js"),
     campgroundRoutes = require("./routes/campgrounds.js"),
     commentRoutes    = require("./routes/comments.js");
 
-
-mongoose.connect('mongodb://localhost:27017/yelp_app', { useNewUrlParser: true });
+// 'mongodb://localhost:27017/yelp_app'
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended : true}));
