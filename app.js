467ac9b1bc039d8479b314b95b3b9edb1f70f94f
David Swiatkiewiez <davidswiatkiewiez@MacBook-Pro-de-David-Swiatkiewiez.local>
2020-03-05 13:22:56 +0100

    corrige port

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comment", commentRoutes);
 
-app.listen(process.env.PORT : 5000, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp app has started!");
 });
